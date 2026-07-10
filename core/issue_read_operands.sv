e3809b207620e56b88e9a58d99f851b6588e713d
telmo <fieuxtelmo@gmail.com>
2026-07-10 14:59:26 +0200

    correction bug de forwarding lors du write back

--- core/issue_read_operands.sv
+++ core/issue_read_operands.sv
@@ -406,11 +406,18 @@ module issue_read_operands
 
       // forwarding in case of dependance between two instr in the same cycle
       for (int unsigned k = 0; k < CVA6Cfg.NrWbPorts; k++) begin
-        if ((we_gpr_i[i] || we_fpr_i[i]) && waddr_i[k] == issue_instr_i[i].rs1 && issue_instr_i[i].rs1 != '0) fu_data_n[i].operand_a = wdata_i[k];
-        if ((we_gpr_i[i] || we_fpr_i[i]) && waddr_i[k] == issue_instr_i[i].rs2 && issue_instr_i[i].rs2 != '0) fu_data_n[i].operand_b = wdata_i[k];
+
+        if ((we_gpr_i[k] || we_fpr_i[k]) && waddr_i[k] == issue_instr_i[i].rs1 && issue_instr_i[i].rs1 != '0)
+            fu_data_n[i].operand_a = wdata_i[k];
+
+        if ((we_gpr_i[k] || we_fpr_i[k]) && waddr_i[k] == issue_instr_i[i].rs2 && issue_instr_i[i].rs2 != '0)
+            fu_data_n[i].operand_b = wdata_i[k];
+
         if (OPERANDS_PER_INSTR == 3 && !issue_instr_i[i].use_imm) begin
-          if ((we_gpr_i[i] || we_fpr_i[i]) && waddr_i[k] == issue_instr_i[i].result && issue_instr_i[i].result != '0) fu_data_n[i].imm = wdata_i[k];
+          if ((we_gpr_i[k] || we_fpr_i[k]) && waddr_i[k] == issue_instr_i[i].result && issue_instr_i[i].result != '0)
+              fu_data_n[i].imm = wdata_i[k];
         end
+
       end
 
       // use the PC as operand a
