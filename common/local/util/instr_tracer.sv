abefdd08c933d51f84726d785ee42105b2e60d6b
Ryaneboutiba <ryane.boutiba@univ-tlse3.fr>
2026-07-15 16:48:43 +0100

    modification de la taille lors de la declaration et debut de la remontee des signaux necessaires de issu_stage

--- common/local/util/instr_tracer.sv
+++ common/local/util/instr_tracer.sv
@@ -34,10 +34,10 @@ module instr_tracer #(
   input logic                    fetch_ack,
   input logic                    issue_ack, // issue acknowledged
   input scoreboard_entry_t       issue_sbe, // issue scoreboard entry
-  input logic [1:0][CVA6Cfg.RegAddrWidth-1:0] waddr, // WB stage
-  input logic [1:0][63:0]        wdata,
-  input logic [1:0]              we_gpr,
-  input logic [1:0]              we_fpr,
+  input logic [CVA6Cfg.NrWbPorts-1:0][CVA6Cfg.RegAddrWidth-1:0] waddr, // WB stage
+  input logic [CVA6Cfg.NrWbPorts-1:0][63:0]        wdata,
+  input logic [CVA6Cfg.NrWbPorts-1:0]              we_gpr,
+  input logic [CVA6Cfg.NrWbPorts-1:0]              we_fpr,
   input scoreboard_entry_t [1:0] commit_instr, // commit instruction
   input logic [1:0]              commit_ack,
   input logic                    st_valid,   // stores - address translation
